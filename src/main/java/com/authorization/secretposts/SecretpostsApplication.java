a547629e0bf0edcd4bbd77826f47c5a40c65b0b0
RocioCMartinez <rym5411@gmail.com>
2023-07-31 19:34:36 -0400

    initial commit

---
+++ src/main/java/com/authorization/secretposts/SecretpostsApplication.java
@@ -0,0 +1,13 @@
+package com.authorization.secretposts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretpostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretpostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authorization/secretposts/SecretpostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authorization.secretposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretpostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
